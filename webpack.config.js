1e409b6c3598a520ea30ca764c006dfde790e94a
Ismael Romero <76606976+Ismael-Romero@users.noreply.github.com>
2022-05-30 12:36:20 -0500

    build app

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,6 @@ module.exports = {
     resolve: { extensions: ['.tsx', '.ts', '.js', '.jsx', '.css','.sass','.less'] },
     output: {
         filename: 'login_build.js',
-        path: path.resolve(__dirname, 'build', 'prod')
+        path: path.resolve(__dirname, 'dist', 'prod')
     }
 }
